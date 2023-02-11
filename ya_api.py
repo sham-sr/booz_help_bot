d5c68c2ed8f596692831496715570711a2f9cf7a
sham-sr <shamsr.rus@gmail.com>
2023-02-11 17:12:16 +0000

    up

--- ya_api.py
+++ ya_api.py
@@ -4,20 +4,20 @@ import json
 
 # переводчик
 def ya_translate(text,target_language='ru'):
-  try:
-    headers = {
-                "Content-Type": "application/json",
-                "Authorization": f'Api-Key {os.getenv("YA_API_KEY")}'
-              }
-    body = {
-    "targetLanguageCode": target_language,
-    "texts": text,
-    "folderId": os.getenv("YA_FOLDER"),
-    }
-    response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
-                              json = body,
-                              headers = headers
-                            )
-    return json.loads(str(response.text))['translations'][0]['text']
-  except:
-    return None
+    try:
+        headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f'Api-Key {os.getenv("YA_API_KEY")}'
+                  }
+        body = {
+        "targetLanguageCode": target_language,
+        "texts": text,
+        "folderId": os.getenv("YA_FOLDER"),
+        }
+        response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
+                                  json = body,
+                                  headers = headers
+                                )
+        return json.loads(str(response.text))['translations'][0]['text']
+    except:
+        return None
